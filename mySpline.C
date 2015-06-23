a83739b77a4eda80d3f4d36585d2bcdf696bd9d3
Anoushka Bose <abose2016@francisparker.org>
2015-06-23 11:16:15 -0700

    randSpline functioning to display all points but spline drawn incorrectly. mySpline is non-functional at the moment.

--- mySpline.C
+++ mySpline.C
@@ -1,29 +1,26 @@
 #include "TSpline.h"
+#define ARRAY_SIZE(array) (sizeof((array))/sizeof((array[0])))
 
+const int size = 5;
+double xarray[5], yarray[5];// empty arrays of size "size". 
 
-// making some arrays
-//const int n = 4;// number of points
-//double Point_x[n] = {0, 1, 1.5, 2};
-//double Point_y[n] = {1.43, 0.368, 0.135, 0.018};
+int seed = 546563;
+TRandom3 *jrand = new TRandom3(seed);// TRandom3 is the best so I'm making an object of the type TRandom3. 
 
- const int n = 10;
-double xarray[n], yarray[n];// empty arrays of size n. 
-
- int seed = 546563;
-    TRandom3 *jrand = new TRandom3(seed);// TRandom3 is the best so I'm making an object of the type TRandom3. 
-
-jrand.RndmArray(n, xarray);// RndmArray(size of array, name of array) fills the arrays with random numbers.
-jrand.RndmArray(n, yarray);
-
+jrand->RndmArray(5, xarray);// RndmArray(size of array, name of array) fills the arrays with random numbers.
+jrand->RndmArray(5, yarray);
 
 // main function with the same name of this file
 // it will be executed when you type ".x interpolation.C" in ROOT
 void mySpline()
 {
 // Section 1. Draw the points on a canvas
+	cout << "Size of x array: " << ARRAY_SIZE(xarray) << endl;  // prints 5 as expected
+	cout << "Size of y array: " << ARRAY_SIZE(yarray) << endl;  // prints 5 as expected
+
    TCanvas *c1 = new TCanvas("c1","interpolation",0,0,1000,800);
 
-   TGraph *g1 = new TGraph(n, xarray, yarray);// just plots points
+   TGraph *g1 = new TGraph(5, xarray, yarray);// just plots points
 
    g1->SetMarkerStyle(20);
    g1->SetMarkerSize(2);
@@ -42,7 +39,7 @@ void mySpline()
                    // http://root.cern.ch/root/html/TGraph.html#TGraph:PaintGraph
 
 // Section 3. Draw the Cubic Spline to the same canvas
-   TSpline3 *sp = new TSpline3("Cubic Spline", xarray, yarray, n, "b2e2", 0, 0);
+   TSpline3 *sp = new TSpline3("Cubic Spline", xarray, yarray, 5, "b2e2", 0, 0);
    // refer to http://root.cern.ch/root/html/Tspline3.html for the usage of TSpline3
    // "b2e2" together with the last two "0" means that the second derivatives 
    // of the begin and end points equal to zero
--- randSpline.C
+++ randSpline.C
@@ -12,38 +12,31 @@ using namespace std;
 //put random numbers into some vectors
 void FillRandVectors(vector< double > &xVector,
                      vector< double > &yVector,
-                     vector< double > &xErrorVector, 	 			     
-					 vector< double > &yErrorVector, int n)
+					 int n)
 {
     //Call TRandom3
 
-    int seed = 65675;
+    int seed = 23382;
     TRandom3 *jrand = new TRandom3(seed);
     for(int i =0; i<n; i=i+1)
     {
         xVector.push_back(jrand->Uniform(20.0));
         yVector.push_back(jrand->Uniform(5.0, 20.0));
-        xErrorVector.push_back(jrand->Uniform(5.0));
-        yErrorVector.push_back(jrand->Uniform(5.0));
     }
-    TGraphErrors *gr = LoadGraphFromVectors(xVector,yVector,xErrorVector,yErrorVector);
+	TGraph *gr = LoadGraphFromVectors(xVector, yVector);
 
     delete jrand;
 }
 
 // make graph from vectors
-TGraphErrors *LoadGraphFromVectors(std::vector< double > xVector, std::vector< double > yVector, std::vector< double > xErrorVector, std::vector< double > yErrorVector)
+TGraph *LoadGraphFromVectors(std::vector< double > xVector, std::vector< double > yVector)
 {
-    int n = xVector.size();
+    int size = xVector.size();
 
-    if((xVector.size()== yVector.size())&&
-            (yVector.size()== xErrorVector.size())&&
-            (xErrorVector.size()== yErrorVector.size()))
+    if(xVector.size()== yVector.size())
     {
         //Create a graph
-        TGraphErrors *gr = new TGraphErrors(n,&xVector[0],
-                                            &yVector[0], &xErrorVector[0],
-                                            &yErrorVector[0]);
+		TGraph *gr = new TGraph(size, &xVector[0], &yVector[0]);
         gr->SetTitle("");
         gr->SetMarkerStyle(20);
         gr->SetMarkerSize(1.2);
@@ -56,8 +49,8 @@ TGraphErrors *LoadGraphFromVectors(std::vector< double > xVector, std::vector< d
     }
     else
     {
-        TGraphErrors *gr0 = new TGraphErrors();
-        return gr0;
+
+		return null;
     }
 }
 
@@ -66,22 +59,19 @@ void randSpline()
 {
     // making vectors
     vector <double> xVector, yVector, xErrorVector, yErrorVector;
-    const int n = 10;
+    const int n = 6;
 
     // filling vectors with random numbers
-    FillRandVectors(xVector, yVector, xErrorVector,
-                    yErrorVector, n);
+    FillRandVectors(xVector, yVector, n);
     // making graph
-    TGraphErrors *gr = LoadGraphFromVectors(xVector, yVector, xErrorVector,
-                                            yErrorVector);
-   
+	TGraph *gr = LoadGraphFromVectors(xVector, yVector);
+
     //Plot
     TCanvas *c1 = new TCanvas("c1","My Awesome Test Graph!!",200,10,700,500);
     gr->Draw("apz");
 
-   
-// Section 3. Draw the Cubic Spline to the same canvas
-   TSpline3 *sp = new TSpline3("Cubic Spline",gr,"b2e2", 0, 0);
+// Draw the Cubic Spline to the same canvas
+   TSpline3 *sp = new TSpline3("Cubic Spline", gr,"b2e2", 0, 0);
    
    sp->SetLineColor(kRed);
    sp->Draw("lsame");
