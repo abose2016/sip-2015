71de78946f2c36f3e0ba5064a4c8ddd1c21ba1d2
julrath <jrathmannbloch@gmail.com>
2015-07-01 11:07:53 -0700

    This version of the code creates some random points and then graphs them against the TMinuit fit. It has been edited by Dr. Biteau to conform to our group's programming conventions.

--- tMinuitFit.C
+++ tMinuitFit.C
@@ -1,6 +1,8 @@
 #include <vector>
 #include <iostream>
+#include <iomanip>
 #include "TMinuit.h"
+#include "TMath.h"
 #include "TCanvas.h"
 #include "TGraphErrors.h"
 #include "TGraph.h"
@@ -9,40 +11,44 @@
 #include "TAxis.h"
 #include "TSystem.h"
 #include "TF1.h"
+#include "TLatex.h"
 #include "TStyle.h"
 #include <sstream>
 
 using namespace std;
 
 //Data definition
-const int nbins = 5;
 vector <double> xVector, yVector, xErrorVector, yErrorVector;
 	
 
 //______________________________________________________________________________
-double func(float x, double *par)
+double func(float x, vector< double > vpar)
 {
-	double value = (((par[0] * x*x) + (par[1] * x) + par[2]));
+	double value = vpar[0] + vpar[1]*x + vpar[2]*x*x;
 	return value;
 }
 
 //______________________________________________________________________________
-double func2(float x, double par0, double par1, double par2, double epar0, double epar1, double epar2)
+double ComputeChi2(vector< double > vpar)
 {
-	double value = (((par0 * x*x) + (par1 * x) + par2));
-	return value;
+	//calculate chisquare
+	double chisq = 0;
+	for (int i = 0; i < (int)yVector.size(); i++) {
+		double delta = (yVector.at(i) - func(xVector.at(i), vpar)) / yErrorVector.at(i);
+		chisq += delta*delta;
+	}
+	return chisq;
 }
 
 //______________________________________________________________________________
 void fcn(int &npar, double *gin, double &f, double *par, int iflag)
 {
-	//calculate chisquare
-	double chisq = 0;
-	for (int i = 0; i < nbins; i++) {
-		double delta = (yVector.at(i) - func(xVector.at(i), par)) / yErrorVector.at(i);
-		chisq += delta*delta;
-	}
-	f = chisq;
+	vector< double > vpar;
+	vpar.push_back(par[0]);
+	vpar.push_back(par[1]);
+	vpar.push_back(par[2]);
+
+	f = ComputeChi2(vpar);
 }
 
 
@@ -73,7 +79,7 @@ TGraphErrors *LoadGraphFromVectorsWithError(vector<double> xVector, vector<doubl
 		(xErrorVector.size() == yErrorVector.size()))
 	{
 		//Create a graph
-		TGraphErrors *gr = new TGraphErrors(n, &xVector[0], &yVector[0], &xErrorVector[0], &yErrorVector[0]);
+		TGraphErrors *gr = new TGraphErrors(n, &xVector[0], &yVector[0], 0, &yErrorVector[0]);
 		gr->SetTitle("");
 		gr->SetMarkerStyle(20);
 		gr->SetMarkerSize(1.2);
@@ -99,58 +105,78 @@ void tMinuitFit()
 
   
 	double seed = 59050830;
-	FillRandVectors(xVector, yVector, xErrorVector, yErrorVector, nbins, seed);
-
-	TCanvas *c1 = new TCanvas("c1", "interpolation", 0, 0, 1000, 800);
+	int npts = 5;
+	FillRandVectors(xVector, yVector, xErrorVector, yErrorVector, npts, seed);
 	TGraphErrors *g1 = LoadGraphFromVectorsWithError(xVector, yVector, xErrorVector, yErrorVector, "X Axis (arbitrary units)", "Y Axis (arbitrary units)");
-	gStyle->SetOptFit(1111);
-	g1->Draw("ap");
 
 	// Set starting values and step sizes for parameters
-	vector<double> vstart, step;
-	vstart.push_back(3);
-	vstart.push_back(1);
-	vstart.push_back(.1);
-	vstart.push_back(.01);
-
-	step.push_back(.1);
-	step.push_back(.1);
-	step.push_back(.01);
-	step.push_back(.01);
-
 	int npar = 3;
-	TMinuit *gMinuit = new TMinuit(npar);  //initialize TMinuit with a maximum of 5 params
-	gMinuit->SetFCN(fcn);
+	vector<double> vstart, vstep;
+	for(int i=0; i<npar; i++)
+	{
+		vstart.push_back(0.);
+		vstep.push_back(.1);
+	}
 
+	TMinuit *myMinuit = new TMinuit(npar);  //initialize TMinuit with a maximum of 5 params
+	myMinuit->SetFCN(fcn);
 	double arglist[10];
 	int ierflg = 0;
 	arglist[0] = 1;
-	gMinuit->mnexcm("SET ERR", arglist, 1, ierflg);
+	myMinuit->mnexcm("SET ERR", arglist, 1, ierflg);
 
-	for (int i = 0; i < 3; i++) {
+	for (int i = 0; i < npar; i++) {
 		stringstream ss;
 		ss<<"a"<<i;
-		gMinuit->mnparm(i, ss.str().c_str(), vstart.at(i), step.at(i), 0, 0, ierflg);
+		myMinuit->mnparm(i, ss.str().c_str(), vstart.at(i), vstep.at(i), 0, 0, ierflg);
 	}
 
 	// Now ready for minimization step
 	arglist[0] = 500;
 	arglist[1] = 1.;
-	gMinuit->mnexcm("MIGRAD", arglist, 2, ierflg);
+	myMinuit->mnexcm("MIGRAD", arglist, 2, ierflg);
 
 	// Print results
 	double amin, edm, errdef;
 	int nvpar, nparx, icstat;
-	gMinuit->mnstat(amin, edm, errdef, nvpar, nparx, icstat);
+	myMinuit->mnstat(amin, edm, errdef, nvpar, nparx, icstat);
 
-	double par0, par1, par2, epar0, epar1, epar2;
-	gMinuit->GetParameter(0, par0, epar0);
-	gMinuit->GetParameter(1, par1, epar1);
-	gMinuit->GetParameter(2, par2, epar2);
+	vector< double > vpar;
+	for(int i=0; i<npar; i++)
+	{
+		double par, epar;
+		myMinuit->GetParameter(i, par, epar);
+		vpar.push_back(par);
+	}
 
 	stringstream form;
-	form << "(" << par0 << "* x*x) + (" << par1 << "* x) + " << par2;
+	form << vpar[0] << "+" << vpar[1] << "*x + " << vpar[2] <<"*x*x";
 	TF1 *minFunc = new TF1("minFunc", form.str().c_str(), 0.0, 20.0);
+
+	//Retrieve the chi2 and number of degrees of freedom
+	double chi2 = ComputeChi2(vpar);
+	int ndf = npts-npar;
+	double prob = TMath::Prob(chi2,ndf);
+
+	//Drawing
+	TLatex tex;
+	tex.SetTextAlign(12);
+	tex.SetTextSize(0.04);
+	tex.SetNDC();
+
+	stringstream slatex1;
+	slatex1<<setprecision(3)<<"#chi^{2} / ndf = "<<chi2<<" / "<<ndf; 
+
+	stringstream slatex2;
+	slatex2<<setprecision(2)<<"P(#chi^{2},ndf) = "<<prob*100<<" %"; 
+
+	TCanvas *c1 = new TCanvas("c1", "interpolation", 0, 0, 1000, 800);
+	c1->cd();
+	g1->Draw("ap");
 	minFunc->Draw("SAME");
+	tex.DrawLatex(0.18,0.21,slatex1.str().c_str());
+	tex.DrawLatex(0.18,0.16,slatex2.str().c_str());
+
+
 }
 
