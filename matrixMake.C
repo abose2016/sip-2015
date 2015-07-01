0ae902638031305c97e00d326358e3022f6fa038
Anoushka Bose <abose2016@francisparker.org>
2015-07-01 13:40:57 -0700

    Cleaned up, functional matrixMake class. Creates a random matrix, inverts it, then multiplies the two to create identity matrix.

--- matrixMake.C
+++ matrixMake.C
@@ -1,71 +1,79 @@
-#ifndef __CINT__
-#include "Riostream.h"
+#include <iostream>
+#include <vector>
 #include "TMatrixD.h"
-#include "TVectorD.h"
-#include "TGraphErrors.h"
-#include "TDecompChol.h"
-#include "TDecompSVD.h"
-#include "TF1.h"
-#endif
+#include "TMath.h"
+#include "TRandom3.h"
 
-using namespace std;
-
-void FillRandVectors(vector<double> &xVector, int n, int seed = 250, double lowerBound= 5, double upperBound= 20)
+//Fill x,y and error vectors with random points from TRandom#
+void FillRandVectors(std::vector< double > &xVector, int n, int seed = 250, double lowerBound = 5, double upperBound = 20)
 {
 	//Call TRandom3
 	TRandom3 *jrand = new TRandom3(seed);
-	for (int i = 0; i<n; i = i + 1)
+	for (int i = 0; i < n; i = i + 1)
 	{
 		xVector.push_back(jrand->Uniform(lowerBound, upperBound));
-		
 	}
 	delete jrand;
 }
 
-
+//Main function - creates randomly populated matrix A, inverts it, and then multiplies the two to create identity matrix
 void matrixMake(){
-	Double_t det1;
-  TMatrixD a(4,4);
-	TMatrixD b(4,4);
-	vector <double> data;
-	FillRandVectors(data, 16);
-	int counter=0;
+	int counter = 0;
+	int matrixSize = 3;
 
+	//Loading random values of matrix into vector labeled data
+	std::vector< double > data;
+	FillRandVectors(data, matrixSize*matrixSize);
 
-	for(int i=0; i<4; i++)
+	//Creating two square matrices
+	TMatrixD a(matrixSize, matrixSize);
+	TMatrixD b(matrixSize, matrixSize);
+
+	//Loading values of data vector into matrices A and B
+	for (int i = 0; i < matrixSize; i++)
 	{
-		for(int j=0; j<4; j++)
+		for (int j = 0; j < matrixSize; j++)
 		{
-			
-			a[i][j]= data.at(counter);
-			b[i][j]= data.at(counter);
+
+			a[i][j] = data.at(counter);
+			b[i][j] = data.at(counter);
 			counter++;
-
 		}
-	} 
+	}
 
-
-	for(int i=0; i<4; i++)
+	//Printing the values of matrix A to console
+	for (int i = 0; i < matrixSize; i++)
+	{
+		for (int j = 0; j < matrixSize; j++)
 		{
-			for(int j=0; j<4; j++)
-			{
-				cout<< a[i][j]<< ",";
-			}
-		cout<< "\n";
-		} 
+			cout << a[i][j] << ",       ";
+		}
+		cout << "\n\n";
+	}
 
+	//Inversion of matrix A and printing new values to console
+	a.Invert();
+	cout << "Matrix A after inversion" << endl;
 
-	a.InvertFast(&det1);
-	cout << "After inversion" <<endl;
-
-	for(int i=0; i<4; i++)
+	for (int i = 0; i < matrixSize; i++)
+	{
+		for (int j = 0; j < matrixSize; j++)
 		{
-			for(int j=0; j<4; j++)
-			{
-				std::cout<< a[i][j]<< ",";
-			}
-		std::cout<< "\n";
-		} 
+			std::cout << setprecision(2) << a[i][j] << ",       ";
+		}
+		std::cout << "\n\n";
+	}
 
-	
+	//Multiplying the inverted matrix by the original (now stored as matrix B) in order to get the identity matrix
+	a *= b;
+	cout << "Inverted matrix A multiplied by its original (identity matrix)" << endl;
+
+	for (int i = 0; i < matrixSize; i++)
+	{
+		for (int j = 0; j < matrixSize; j++)
+		{
+			std::cout << setprecision(2) << a[i][j] << ",   ";
+		}
+		std::cout << "\n\n";
+	}
 }
