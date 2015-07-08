7cdcf65110fa2c48228d141e2c274a30766ccdd1
julrath <jrathmannbloch@gmail.com>
2015-07-08 11:23:13 -0700

    Draws a B-Spline between a set of randomized points. Somewhat cleaned up version, but still needs work.

--- splines/bSplineGSLDemoV2.C
+++ splines/bSplineGSLDemoV2.C
@@ -11,15 +11,16 @@
 #include "TGraph.h"
 #include "TAxis.h"
 #include <vector>
+#include <iostream>
 #include "TRandom3.h"
+
 
 void bSplineGSLDemoV2 ()
 {
-	const size_t nControl = 15;
-	int nControlInt = 15;
-	const size_t ncoeffs = 12;
-	const size_t nbreak = ncoeffs - 2;
-	size_t i, j;
+	const int n = 15;
+	const int ncoeffs = 12;
+	const int nbreak = ncoeffs-2;
+	int i, j;
 	gsl_bspline_workspace *bw;
 	gsl_vector *B;
 	gsl_vector *c, *w;
@@ -27,44 +28,43 @@ void bSplineGSLDemoV2 ()
 	gsl_matrix *X, *cov;
 	gsl_multifit_linear_workspace *mw;
 	double chisq, Rsq, dof, tss;
-	double splineStep = 0.1;
-	int nValues = nControlInt/splineStep;
-	double xOrigin[nControlInt], yOrigin[nControlInt], xValues[nValues], yValues[nValues];
+	vector<double> xValues, yValues, xOrigin, yOrigin;
+	
  
 	/* allocate a cubic bspline workspace (k = 4) */
 	bw = gsl_bspline_alloc(4, nbreak);
 	B = gsl_vector_alloc(ncoeffs);
 
-	xControl = gsl_vector_alloc(nControl);
-	yControl = gsl_vector_alloc(nControl);
-	X = gsl_matrix_alloc(nControl, ncoeffs);
+	xControl = gsl_vector_alloc(n);
+	yControl = gsl_vector_alloc(n);
+	X = gsl_matrix_alloc(n, ncoeffs);
 	c = gsl_vector_alloc(ncoeffs);
-	w = gsl_vector_alloc(nControl);
+	w = gsl_vector_alloc(n);
 	cov = gsl_matrix_alloc(ncoeffs, ncoeffs);
-	mw = gsl_multifit_linear_alloc(nControl, ncoeffs);
-	TRandom3 jrand;
+	mw = gsl_multifit_linear_alloc(n, ncoeffs);
+ 	int seed = 7898;
+	TRandom3 *jrand = new TRandom3(seed);
 
-	for (i = 0; i < nControl; ++i)
+	for (i = 0; i < n; ++i)
 			 {
 				double sigma;
-				double xi = (nControlInt / (nControlInt - 1)) * i;
-				double yi = jrand.Uniform(20);
+				double xi = (15.0 / (n - 1)) * i;
+				double yi = jrand->Uniform(20);
 				sigma = 0.1 * yi;
-				gsl_vector_set(xControl, i, xi);
-				xOrigin[i] = xi;
-				gsl_vector_set(yControl, i, yi);
-				yOrigin[i] = yi;
+				 gsl_vector_set(xControl, i, xi);
+				xOrigin.push_back(xi);
+				 gsl_vector_set(yControl, i, yi);
+				yOrigin.push_back(yi);
 				gsl_vector_set(w, i, 1.0 / (sigma * sigma));
-
-				 printf("%f %f\n", xi, yi);
+				std::cout<< xi<<"   "<<yi<< std::endl;
 			 }
 
 	
 	/* use uniform breakpoints on [0, 15] */
-	gsl_bspline_knots_uniform(0.0, nControlInt, bw);
+	gsl_bspline_knots_uniform(0.0, 15.0, bw);
 
 	/* construct the fit matrix X */
-	for (i = 0; i < nControl; ++i)
+	for (i = 0; i < n; ++i)
 	{
 		double xi = gsl_vector_get(xControl, i);
 
@@ -82,7 +82,7 @@ void bSplineGSLDemoV2 ()
 	/* do the fit */
 	gsl_multifit_wlinear(X, w, yControl, c, cov, &chisq, mw);
 
-	dof = nControl - ncoeffs;
+	dof = n - ncoeffs;
 	tss = gsl_stats_wtss(w->data, 1, yControl->data, 1, yControl->size);
 	Rsq = 1.0 - chisq / tss;
 
@@ -90,22 +90,22 @@ void bSplineGSLDemoV2 ()
 		                chisq / dof, Rsq);
 
 	/* output the smoothed curve */
-	
+	{
 		double xi, yi, yerr;
-		int nSpline = int((xOrigin[nControlInt-1]-xOrigin[0])/splineStep);
 		printf("BREAK\n\n\n\n\n");	
 		
-		for (xi = 0.0; xi < nControl; xi += splineStep)
+		printf("#m=1,S=0\n");
+		for (xi = 0.0; xi < 15.0; xi += 0.1)
 		{
 			gsl_bspline_eval(xi, B, bw);
 			gsl_multifit_linear_est(B, c, cov, &yi, &yerr);
-			int index = (int)xi*10;
-			xValues[index] = xi;
-			yValues[index] = yi;
+			xValues.push_back(xi);
+			yValues.push_back(yi);
 
-			printf("%f %f\n", xi, yi);	
+			std::cout<< xi<< "   " << yi << std::endl;
 		}
-	
+	}
+
 	gsl_bspline_free(bw);
 	gsl_vector_free(B);
 	gsl_vector_free(xControl);
@@ -115,18 +115,21 @@ void bSplineGSLDemoV2 ()
 	gsl_vector_free(w);
 	gsl_matrix_free(cov);
 	gsl_multifit_linear_free(mw);
+	
 
-    TGraph *grControlPoints = new TGraph(nControlInt, xOrigin, yOrigin);
-    grControlPoints->SetMarkerStyle(20);
+	//load graph
+	int n1= xValues.size(), n2= xOrigin.size();
+	TGraph *gr = new TGraph(n2, &xOrigin[0], &yOrigin[0]);
+  gr->SetMarkerStyle(20);
 
-    TGraph *grSpline = new TGraph(nSpline, xValues, yValues);
-    grSpline->SetTitle("");
-    grSpline->GetXaxis()->SetTitle("X-axis  [A.U.]");
-    grSpline->GetYaxis()->SetTitle("Y-axis  [A.U.]");
+  TGraph *gr1 = new TGraph(n1, &xValues[0], &yValues[0]);
+  gr1->SetTitle("");
+  gr1->GetXaxis()->SetTitle("X-axis  [A.U.]");
+  gr1->GetYaxis()->SetTitle("Y-axis  [A.U.]");
 
-    //plot
-    TCanvas *c1 = new TCanvas("c1", "Graph", 200, 10, 700, 500);
-    c1->cd();
-    grSpline->Draw("al");
-    grControlPoints->Draw("same p");
+	//plot
+	TCanvas *c1 = new TCanvas("c1", "Graph", 200, 10, 700, 500);
+ 	c1->cd();
+	gr1->Draw("al");
+	gr->Draw("SAME p");
 } 
