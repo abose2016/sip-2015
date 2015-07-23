56036b802e3bfdee1a88f49f7538a6dceee025ec
julrath <jrathmannbloch@gmail.com>
2015-07-23 15:11:45 -0700

    Working linear interpolator. To be included in multiple spline code.

--- splines/linearInterpolation.C
+++ splines/linearInterpolation.C
@@ -31,7 +31,7 @@ void FillRandVectors(int nPoints, vector<double> &xVector, vector< double > &yVe
 }
 //_____________________________________________________________________________
 
-vector <vector<double> > linearInterpolation( vector<double> xData, vector<double> yData, double stepSpline = 0.1)
+vector <vector<double> > linearInterpolation( vector<double> xData, vector<double> yData, double stepSpline = 0.01)
 {
 	vector <vector<double> > linearInterp;
 	linearInterp.push_back( vector<double> () );//x
@@ -49,6 +49,7 @@ vector <vector<double> > linearInterpolation( vector<double> xData, vector<doubl
 			double x1 = xData.at(i+1);
 			double y0 = yData.at(i);
 			double y1 = yData.at(i+1);
+			std::cout << "x0: " << x0 << " x1: " << x1 << " y0: " << y0 << " y1: " << y1 << endl;
 			vector< double > xSpline, ySpline;
 
 			for (double j = xData.at(i); j < xData.at(i+1); j += stepSpline)
@@ -57,12 +58,13 @@ vector <vector<double> > linearInterpolation( vector<double> xData, vector<doubl
 					double m = (y1 - y0)/(x1 - x0);
 					double b = -m*x0 + y0;
 					double y = m * (j) + b; 
+					std::cout << "M: " << m << " B: " << b << " Y: " << y << endl;
+					std::cout << "J: " << j << endl;
 					linearInterp[0].push_back(j);
 					linearInterp[1].push_back(y); 
 			}
 		} 
 	} 
-
 
 	return linearInterp;
 }
@@ -74,9 +76,9 @@ void grapher()
 	FillRandVectors(nPoints, xData, yData);
 	vector <vector<double> > interpPoints = linearInterpolation(xData, yData);
 
-	TGraph *lin = new TGraph(nPoints, &interpPoints[0][0], &interpPoints[1][0]);
+	TGraph *lin = new TGraph((int)interpPoints[0].size(), &interpPoints[0][0], &interpPoints[1][0]);
 	lin->SetMarkerColor(kBlue);
-	lin->SetMarkerStyle(20);
+	lin->SetMarkerStyle(2);
 
 	TGraph *data = new TGraph(nPoints, &xData[0], &yData[0]);
 	data->SetMarkerColor(kRed);
@@ -84,8 +86,8 @@ void grapher()
 
 	TCanvas *c1 = new TCanvas("c1", "Linear");
 	c1->cd();
-//	lin->Draw("ap");
 	data->Draw("ap"); 
+	lin->Draw("same l");
 }
 
 	
